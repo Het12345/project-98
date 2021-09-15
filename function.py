470e594d100f8d0e896a7560764d38ce3fb68201
Het12345 <90767991+Het12345@users.noreply.github.com>
2021-09-15 17:58:47 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,2 @@
+f=open("D:/coding folder/lec-91/text.txt")
+f.read()
---
+++ os.py
@@ -0,0 +1,5 @@
+import os
+import shutil
+source="D:/coding folder/lec-91/backup"
+destinatiion="D:/coding folder/lec-91/python"
+copyied=shutil.move(source,destinatiion)
